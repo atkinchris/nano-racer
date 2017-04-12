75cf02c4e5f5045dc116736ff474b0a6aedfb064
Chris Atkin <atkinchris@gmail.com>
2017-04-12 08:32:21 +0100

    Improved movement

--- src/actors/boat.js
+++ src/actors/boat.js
@@ -1,30 +1,45 @@
-import { Sprite, Physics } from 'phaser'
+import { Sprite } from 'phaser'
+
+const increase = (number, amount) => {
+  const multiplier = number < 0 ? -1 : 1
+  const ret = multiplier * (Math.abs(number) + amount)
+
+  return ret * number <= 0 ? 0 : ret
+}
 
 class Boat extends Sprite {
-  constructor(game, x, y) {
-    super(game, x, y, 'boat')
+  constructor({ game, x, y, asset }) {
+    super(game, x, y, asset)
 
-    this.cursors = game.input.keyboard.createCursorKeys()
-    this.body = new Physics.P2.Body(game, this)
+    this.anchor.setTo(0.5)
+    this.velocity = 0
+    this.angularVelocity = 0
+    this.turningSpeed = 8
+  }
 
-    game.add.existing(this)
+  initialize() {
+    this.game.physics.p2.enable(this, false)
+    this.cursors = this.game.input.keyboard.createCursorKeys()
   }
 
   update() {
-    const { body, cursors } = this
-
-    if (cursors.left.isDown) {
-      body.rotateLeft(50)
-    } else if (cursors.right.isDown) {
-      body.rotateRight(50)
+    if (this.cursors.up.isDown && this.velocity <= 400) {
+      this.velocity += 7
+    } else if (this.cursors.down.isDown && this.velocity >= -400) {
+      this.velocity -= 7
     } else {
-      body.setZeroRotation()
+      this.velocity = increase(this.velocity, -7)
     }
 
-    if (cursors.up.isDown) {
-      body.moveForward(100)
-    } else if (cursors.down.isDown) {
-      body.moveBackward(20)
+    this.body.velocity.x = this.velocity * Math.cos((this.angle - 90) * 0.01745)
+    this.body.velocity.y = this.velocity * Math.sin((this.angle - 90) * 0.01745)
+
+    if (this.cursors.left.isDown) {
+      this.body.angularVelocity = -1 * this.turningSpeed * (this.velocity / 1000)
+    } else if (this.cursors.right.isDown) {
+      this.body.angularVelocity = this.turningSpeed * (this.velocity / 1000)
+    } else {
+      this.body.angularVelocity = 0
     }
   }
 }
--- src/states/game.js
+++ src/states/game.js
@@ -14,13 +14,27 @@ class GameState extends Phaser.State {
     game.stage.backgroundColor = '#71c5cf'
     game.physics.startSystem(Phaser.Physics.P2JS)
     game.time.advancedTiming = true
+    game.world.setBounds(0, 0, 3000, 3000)
 
-    const boat = new Boat(game, 100, 100)
+    const boat = new Boat({
+      game: this,
+      x: this.world.centerX,
+      y: this.world.centerY,
+      asset: 'boat',
+    })
+
     window.boat = boat
+    this.boat = boat
+
+    this.game.add.existing(boat)
+    boat.initialize()
+
+    game.camera.follow(boat, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1)
   }
 
   render() {
     this.game.debug.text(this.game.time.fps || '--', 2, 14, '#00ff00')
+    this.game.debug.spriteInfo(this.boat, 32, 32)
   }
 }
 
