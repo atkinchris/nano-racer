394ecddbffed4723efb977b20ab240aa2d85774c
Chris Atkin <atkinchris@gmail.com>
2017-04-17 16:44:30 +0100

    Simplify component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import 'pixi'
 import 'p2'
+import Phaser from 'phaser'
 
-import Game from './components/Game'
+import GameState from './states/game'
 
-window.game = new Game('root')
+class Game extends Phaser.Game {
+  constructor() {
+    super(448, 496, Phaser.AUTO, 'root', null)
+
+    this.state.add('Game', GameState)
+    this.state.start('Game')
+  }
+}
+
+window.game = new Game()
